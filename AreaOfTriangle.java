01103ad8f3f381aa06f59b144bf4c41bcd034c96
Riley <rileywagner@student.olympic.edu>
2022-05-09 17:33:27 -0700

    added documentation

--- AreaOfTriangle.java
+++ AreaOfTriangle.java
@@ -11,20 +11,20 @@ public class AreaOfTriangle {
 
 	public static void triangleArea(Scanner console, double a, double b, double c){
 		if(console == null) {
-			throw new IllegalArgumentException("Scanner does not exist.");
+			throw new IllegalArgumentException("Scanner does not exist.");		//throws exception if scanner argument is null.
 		}
 		System.out.print("Enter a value for \"a\":");
-			a = console.nextDouble();
+			a = console.nextDouble();				//prompts for user input for side a of the triangle.
 				if(a <= 0) {
 					throw new IllegalArgumentException("Please enter a value greater than 0.");
 				}
 		System.out.print("Enter a value for \"b\":");
-			b = console.nextDouble();
+			b = console.nextDouble();			//prompts for user input for side b of the triangle.
 				if(b <= 0) {
 					throw new IllegalArgumentException("Please enter a value greater than 0.");
 				}
 		System.out.print("Enter a value for \"c\":");
-			c = console.nextDouble();
+			c = console.nextDouble();					//prompts for user input for side c of the triangle.
 				if(c <= 0) {
 					throw new IllegalArgumentException("Please enter a value greater than 0.");
 				}
@@ -32,10 +32,10 @@ public class AreaOfTriangle {
 			System.out.println();
 
 			var s =(a + b + c) / 2;
-				var area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+				var area = Math.sqrt(s * (s - a) * (s - b) * (s - c));		//calculates area of triangle using Heron's Formula.
 
 		if(Double.isNaN(area)) {
-			throw new IllegalArgumentException("Given values cannot make a triangle.");
+			throw new IllegalArgumentException("Given values cannot make a triangle.");		//exception thrown if sides don't make a triangle.
 		}
 		System.out.print(area);
 	}
